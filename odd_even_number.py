de94e8c30cc7dc621ba5b09418ccad5e4b03ac84
ductungsis <macair@MacAirs-MacBook-Air.local>
2022-10-25 16:33:32 +0700

    odd and even number

---
+++ odd_even_number.py
@@ -0,0 +1,12 @@
+import turtle
+import random
+
+
+number = int(input("nhap vao so nguyen:"))
+if (number%2 == 0):
+    message = "even"
+else:
+    message ="odd"
+print(message)   
+
+turtle.done()
